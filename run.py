d02870d5f451c4f6934b59d30401baa3e458f990
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-09-29 19:06:31 +0000

    Collect sales data last 5 markets in list of columns

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ def validate_data(values):
 """
 I love this so I keep it, those two where REFACTOR -ED
 Refactoring mean that both where merged into the function
- called update_worksheet
+called update_worksheet
 
 def update_sales_worksheet(data):
     "" "
@@ -108,6 +108,20 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Collects collumns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and retutns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
 
 def main():
     """
@@ -122,4 +136,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
